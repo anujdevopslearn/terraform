7bce45a097d6467915350b6ce844d19bb0a051c7
Anuj Sharma <anuj_sharma401@yahoo.com>
2025-01-12 22:35:45 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -29,6 +29,7 @@ variable "instance_details" {
   }))
   default = [
     { instance_name = "Ubuntu 24.04", ami_id = "ami-0e2c8caa4b6378d8c" },
+    { instance_name = "Amazon Linux 2", ami_id = "ami-05576a079321f21f8" },
     { instance_name = "Amazon Linux", ami_id = "ami-05576a079321f21f8" }
   ]
 }
