fc0c50a3440595780daa18e3262b79afb25b06dc
Anuj Sharma <anuj_sharma401@yahoo.com>
2025-03-24 15:50:58 +0530

    Update and rename server.tf to rds.tf

---
+++ rds.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_instance" "example" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "admin"
+  password             = "password"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+}
